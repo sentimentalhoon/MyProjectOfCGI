4ac4e0e3b2cc5894b4ac5eeb34af9f9ccb0da740
ByeongHoonChoi <sentimentalhoon@gmail.com>
2023-07-09 10:08:29 +0900

    메소드명 변경

--- JavaFestival/question_bonus_06.java
+++ JavaFestival/question_bonus_06.java
@@ -4,22 +4,22 @@ public class question_bonus_06 {
     public static void main(String[] args) {
         int base = 2, n = 3;
         System.out.println("결과 확인[Math.pow 사용] : " + powerN(base, n));
-        System.out.println("결과 확인[재귀 사용] : " + CalculatePower(base, n));
-        System.out.println("결과 확인[For 사용] : " + calculatePowerForFunc(base, n));
+        System.out.println("결과 확인[재귀 사용] : " + powerNRecursion(base, n));
+        System.out.println("결과 확인[For 사용] : " + powerNFor(base, n));
     }
 
     private static int powerN(int base, int n) {
         return (int) (Math.pow(base, n));
     }
 
-    private static int CalculatePower(int num, int pow) {
+    private static int powerNRecursion(int num, int pow) {
         if (pow == 0)
             return 1;
         else
-            return num * CalculatePower(num, pow - 1);
+            return num * powerN(num, pow - 1);
     }
 
-    private static int calculatePowerForFunc(int num, int power) {
+    private static int powerNFor(int num, int power) {
         int answer = 1;
         if (num > 0 && power == 0) {
             return answer;
