31ddbbda8d8995367f8c7bda4f820a1354681012
ByeongHoonChoi <sentimentalhoon@gmail.com>
2023-07-20 11:26:04 +0900

    edit

--- src/Utils/InputScanner.java
+++ src/Utils/InputScanner.java
@@ -7,16 +7,7 @@ public class InputScanner {
     private InputScanner() {
     }
 
-    private static InputScanner _instance = null;
-
-    public static InputScanner getInstance() {
-		if (_instance == null) {
-			_instance = new InputScanner();
-		}
-		return _instance;
-	}
-
-    public Scanner getScanner(){
+    public static Scanner getScanner(){
         Scanner sc = new Scanner(System.in);
         return sc;
     }
